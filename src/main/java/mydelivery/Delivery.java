53c8461c67cdcd558129277d524c097a7b4696c5
bindae999 <bindae999@naver.com>
2020-10-28 18:24:18 +0900

    first commit

---
+++ src/main/java/mydelivery/Delivery.java
@@ -0,0 +1,53 @@
+package mydelivery;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+import java.util.List;
+
+@Entity
+@Table(name="Delivery_table")
+public class Delivery {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long deliveryId;
+
+    private Long orderId;
+    private String orderStatus;
+
+    @PostUpdate
+    public void onPostUpdate(){
+        Delivered delivered = new Delivered();
+        BeanUtils.copyProperties(this, delivered);
+        delivered.publishAfterCommit();
+
+
+    }
+
+
+    public Long getDeliveryId() {
+        return deliveryId;
+    }
+
+    public void setDeliveryId(Long deliveryId) {
+        this.deliveryId = deliveryId;
+    }
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+
+
+
+}
---
+++ src/main/java/mydelivery/DeliveryRepository.java
@@ -0,0 +1,8 @@
+package mydelivery;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface DeliveryRepository extends PagingAndSortingRepository<Delivery, Long>{
+
+
+}
